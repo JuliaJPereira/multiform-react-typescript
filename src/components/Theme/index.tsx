706efb1295cc0ba92e9047826b7db8b0b01c1429
Julia Pereira <juliapereira.odc@gmail.com>
2022-09-02 19:19:04 -0300

    Create Theme

---
+++ src/components/Theme/index.tsx
@@ -0,0 +1,26 @@
+import { ReactNode } from 'react';
+import * as C from './styles';
+import { Header } from '../Header';
+
+type Props = {
+    children: ReactNode;
+}
+
+export const Theme = ({children}: Props) => {
+    return (
+        <C.Container>
+            <C.Area>
+                <Header />
+
+                <C.Steps>
+                    <C.Sidebar>
+                        ...
+                    </C.Sidebar>
+                    <C.Page>
+                        {children}
+                    </C.Page>
+                </C.Steps>
+            </C.Area>
+        </C.Container>
+    )
+}
