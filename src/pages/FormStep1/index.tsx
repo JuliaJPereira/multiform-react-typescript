d4acb2fa2bc7446fd09c0100915f5173410bd42b
Julia Pereira <juliapereira.odc@gmail.com>
2022-09-02 19:19:22 -0300

    Create Step1

--- src/pages/FormStep1/index.tsx
+++ src/pages/FormStep1/index.tsx
@@ -1,9 +1,50 @@
-import * as C from './style';
+import { useHistory } from "react-router-dom";
+import * as C from "./style";
+import { useForm, FormActions } from "../../contexts/FormContext";
+import { Theme } from "../../components/Theme";
+import { ChangeEvent, useEffect } from "react";
 
 export const FormStep1 = () => {
-    return(
-        <C.Container>
-            Passo 1
-        </C.Container>
-    )
-}
+  const history = useHistory();
+  const { state, dispatch } = useForm();
+
+  useEffect(() => {
+    dispatch({
+      type: FormActions.setCurrentStep,
+      payload: 1,
+    });
+  });
+
+  const handleNextStep = () => {
+    state.name !== "" ? history.push("/step2") : alert("Preencha os dados");
+  };
+
+  const handleChangeName = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch({
+      type: FormActions.setName,
+      payload: e.target.value,
+    });
+  };
+
+  return (
+    <Theme>
+      <C.Container>
+        <p>Passo 1/3</p>
+        <h1>Vamos começar com o seu nome</h1>
+        <p>Preencha o campo abaixo com o seu nome completo.</p>
+        <hr />
+        <label>
+          Seu nome completo
+          <input
+            type="text"
+            autoFocus
+            value={state.name}
+            onChange={handleChangeName}
+          />
+        </label>
+
+        <button onClick={handleNextStep}>Próximo</button>
+      </C.Container>
+    </Theme>
+  );
+};
